5c796b62e22ef62e599d495cd24636a233609335
Homer Riva-Cambrin <neithercouldterry@gmail.com>
2024-10-24 12:28:05 -0600

    added better spin lock waiting allowing for more processes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use std::{sync::{atomic::{AtomicBool, Ordering}, Arc}, thread, time::Duration};
 
 
-pub const ROUNDS: usize = 100;
-pub const PROCESSES: usize = 1;
+pub const ROUNDS: usize = 10;
+pub const PROCESSES: usize = 10;
 
 
 /// How many milliseconds should each process hold
 /// the critical section for?
+/// 
+/// Note, this should be at least one, or else
+/// if all the cores operate on the same prcess
+/// this will not properly trip petersons.
 pub const CRITICAL_SECTION_TIME_MS: u64 = 1;
+
+
+/// Spin lock waiting time, this can make the process
+/// a little bit more efficient as it prevents the costly
+/// checking happening which prevents the OS from rescheduling
+/// the threads.
+/// 
+/// This allows for hundreds of processes to compete.
+pub const SPIN_LOCK_WAITING_TIME_MS: u64 = 1;
 
 /// To watch the program experience data races
 /// turn this off.
@@ -59,12 +72,20 @@ pub fn exists(i: usize, k: usize, flag: &mut [i32; PROCESSES], turn: &mut [usize
 pub fn contender(i: usize, flag: &mut [i32; PROCESSES], turn: &mut [usize; PROCESSES - 1], potato: HotPotato) {
    
     for k in 0..PROCESSES - 1 {
+        // Process i is competing at level k
         flag[i] = k as i32;
+        // I believe it is my turn at level k
         turn[k] = i;
         
         
+        // Is there a process competing at higher levels and at the same level?
         while ENABLE_PETERSON && exists(i, k, flag, turn) {
-            // Busy wait
+            // Busy wait, this is just a spin lock.
+            // Highly inefficient-- although will hand
+            // over control really fast. We can really easily improve this
+            // by just putting a condvar here but that would 
+            // complicate the example.
+            thread::sleep(Duration::from_millis(SPIN_LOCK_WAITING_TIME_MS));
         }
 
         
@@ -74,6 +95,8 @@ pub fn contender(i: usize, flag: &mut [i32; PROCESSES], turn: &mut [usize; PROCE
     potato.hold();
     thread::sleep(Duration::from_millis(CRITICAL_SECTION_TIME_MS));
     potato.release();
+
+    // Process i is no longer competing.
     flag[i] = -1;
 }
 
@@ -88,7 +111,6 @@ pub fn contender(i: usize, flag: &mut [i32; PROCESSES], turn: &mut [usize; PROCE
 pub fn create_shared_memory<T>(val: T) -> *mut T {
     Box::leak(Box::new(val)) as *mut T
 }
-
 
 
 fn main() {
@@ -123,6 +145,7 @@ fn main() {
         handle.join().unwrap();
     }
 
+    // Free up memory.
     unsafe {
         std::ptr::drop_in_place(flag);
         std::ptr::drop_in_place(turn);
